7cf39b969c75c0bc2a8ef9b805c44016f2272f74
Jaybie <114408008+jaybiemalone@users.noreply.github.com>
2024-10-14 21:33:29 -0800

    Add files via upload

--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,12 @@
-// Get the button and sidebar elements
-const togglesidebtn = document.getElementById('togglesidebtn');
-const sidebar = document.getElementById('show');
+const sidebtn = document.getElementById("sidebtn");
+const sideshow = document.getElementById("sideshow");
 
-// Add click event listener to the button
-togglesidebtn.addEventListener('click', function() {
-    // Toggle the 'show-side' class on the sidebar
-    sidebar.classList.toggle('show-side');
+sidebtn.addEventListener("click", Event =>{
+
+    if(sideshow.style.visibility === "hidden"){
+        sideshow.style.visibility ="visible";
+    }
+    else{
+        sideshow.style.visibility = "hidden";
+    }
 });
